772ec9cbe03d2cf594abc645b1d7a60bffb69bff
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-10 03:34:42 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   
   name_hash.each do |key,value|
-    #if the lowest_value is in it's intial state  
-    #or if the value of the current key is less than the lowest_value
-    #then: set the lowest_value to value and lowest_key to key
+  
     if lowest_value == 0 || value < lowest_value
       lowest_value = value 
       lowest_key = key
@@ -25,9 +23,6 @@ key_for_min_value(ikea)
 
 
 
-
-#veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
-#key_for_min_value(veggies)
 
 
 
